e78eeb7ecb95b4ea3b66d08110ac429d7a336dfb
Joel Falcou <joel.falcou@lri.fr>
2018-06-25 20:05:48 +0200

    Use constexpr instead of volatile in type tests

--- include/tts/tests/types.hpp
+++ include/tts/tests/types.hpp
@@ -18,49 +18,37 @@
 /*!
   @ingroup group-unit
   @brief Check for type equality
-
   For a given types @c T and  @c Type, consider the test successful if and only if
   @c std::is_same<T,Type> evaluates to @true_.
-
-  @par Usage:
-
-  @snippet test/unit/types.cpp type_is
-
-  @param T    First type to compare
   @param Type Second type to compare
 **/
 #define TTS_TYPE_IS(T, Type)                                                                        \
 do                                                                                                  \
 {                                                                                                   \
-  volatile bool tts_var_b = std::is_same< TTS_REMOVE_PARENS(Type)                                   \
-                                        , TTS_REMOVE_PARENS(T)                                      \
-                                        >::value;                                                   \
-  if( tts_var_b )                                                                                   \
-    TTS_PASS (   "Expecting " << TTS_STRING(TTS_REMOVE_PARENS(T))                                   \
-                <<  " == " << tts::type_id<TTS_REMOVE_PARENS(Type)>()                               \
-                );                                                                                  \
-  else                                                                                              \
-    TTS_FAIL (   "Expecting " << TTS_STRING(TTS_REMOVE_PARENS(T))                                   \
-                <<  " == " << tts::type_id<TTS_REMOVE_PARENS(Type)>()                               \
-                <<  " found " << tts::type_id<TTS_REMOVE_PARENS(T)>()                               \
-                <<  " instead"                                                                      \
-                );                                                                                  \
+  constexpr auto check = std::is_same_v<TTS_REMOVE_PARENS(Type), TTS_REMOVE_PARENS(T)>;             \
+  if constexpr( check )                                                                             \
+  {                                                                                                 \
+    TTS_PASS(     "Expecting " << TTS_STRING(TTS_REMOVE_PARENS(T))                                  \
+              <<  " == "       << tts::type_id<TTS_REMOVE_PARENS(Type)>()                           \
+            );                                                                                      \
+  }                                                                                                 \
+                                                                                                    \
+  if constexpr( !check )                                                                            \
+  {                                                                                                 \
+    TTS_FAIL(     "Expecting " << TTS_STRING(TTS_REMOVE_PARENS(T))                                  \
+              <<  " == "       << tts::type_id<TTS_REMOVE_PARENS(Type)>()                           \
+              <<  " found "    << tts::type_id<TTS_REMOVE_PARENS(T)>()                              \
+              <<  " instead"                                                                        \
+            );                                                                                      \
+  }                                                                                                 \
 } while( ::tts::detail::is_false() )                                                                \
 /**/
 
 /*!
   @ingroup group-unit
   @brief Check for type equality
-
   For a given expression @c Expression and a given type @c Type, consider the test successful if
   and only if @c std::is_same<decltype(Expression),Type> evaluates to @true_ .
-
-  @par Usage:
-
-  @snippet test/unit/types.cpp expr_is
-
-  @param Expression Expression to test type
-  @param Type       Second type to compare
 **/
 #define TTS_EXPR_IS(Expression, Type)                                                               \
 TTS_TYPE_IS(decltype( TTS_REMOVE_PARENS(Expression)), Type)                                         \
