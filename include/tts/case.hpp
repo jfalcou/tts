c05fce8e0208493fd77a839caaf93aa56bdfcd2f
Joel Falcou <joel.falcou@lri.fr>
2019-11-30 23:22:02 +0100

    Undoxyfile header guards

--- include/tts/case.hpp
+++ include/tts/case.hpp
@@ -1,11 +1,11 @@
 //==================================================================================================
-/**
+/*
   TTS - Tiny Test System
   Copyright 2018 Joel FALCOU
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
-**/
+*/
 //=====================================================================================================
 #ifndef TTS_CASE_HPP_INCLUDED
 #define TTS_CASE_HPP_INCLUDED
