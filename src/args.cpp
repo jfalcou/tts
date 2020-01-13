c61d0707c3d0342657efd3ed3f009a726f993664
Joel Falcou <joel.falcou@lri.fr>
2020-01-13 18:53:19 +0100

    Correct Ranges behavior

--- src/args.cpp
+++ src/args.cpp
@@ -20,13 +20,13 @@ namespace tts::detail
 {
   args_map::args_map()  : disable_colors_{false}
                         , report_pass_{false}
+                        , has_ulp_(false)
                         , repetition_{1}
                         , count_{10000}
                         , order_{""}
                         , seed_{1}
-  {
-
-  }
+                        , ulpmax_{2.0}
+  {}
 
   TTS_API void args_map::update(int argc, char **argv) const
   {
@@ -48,6 +48,9 @@ namespace tts::detail
                   ( "c,count"    , "Set size of experiments (default is 10000)"
                   , cxxopts::value<unsigned int>()
                   )
+                  ( "u,ulpmax"    , "Set global failure ulp threshold (default is 1.0)"
+                  , cxxopts::value<double>()
+                  )
                   ( "h,help"      , "Display this help message");
 
     auto result = options.parse(argc, argv);
@@ -61,9 +64,12 @@ namespace tts::detail
     disable_colors_ = result.count("no-color") != 0;
     report_pass_    = result.count("pass") != 0;
 
+    has_ulp_ = result.count("ulpmax");
+
     if( result.count("count")   ) count_      = result["count"].as<unsigned int>();
     if( result.count("repeat")  ) repetition_ = result["repeat"].as<unsigned int>();
     if( result.count("order")   ) order_      = result["order" ].as<std::string>();
+    if( result.count("ulpmax")  ) ulpmax_     = result["ulpmax"].as<double>();
     if( result.count("seed")    )
     {
       seed_ = result["seed"].as<unsigned int>();
