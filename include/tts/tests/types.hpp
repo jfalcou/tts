4707097e8a9df403b2a50262db065ef274f75b2b
Joel Falcou <joel.falcou@lri.fr>
2020-03-18 14:51:55 +0100

    Improved type tests output in case of failure

--- include/tts/tests/types.hpp
+++ include/tts/tests/types.hpp
@@ -37,14 +37,15 @@
     constexpr auto check = std::is_same_v<TTS_REMOVE_PARENS(Type), TTS_REMOVE_PARENS(T)>;          \
     if constexpr(check)                                                                            \
     {                                                                                              \
-      TTS_PASS("Expecting " << ::tts::detail::white_(TTS_STRING(TTS_REMOVE_PARENS(T))) << " == "   \
+      TTS_PASS("Expecting " << ::tts::detail::white_(TTS_STRING(TTS_REMOVE_PARENS(T))) << " to be "\
                             << ::tts::detail::green_(tts::type_id<TTS_REMOVE_PARENS(Type)>()));    \
     }                                                                                              \
                                                                                                    \
     if constexpr(!check)                                                                           \
     {                                                                                              \
-      TTS_FAIL("Expecting " << ::tts::detail::white_(tts::type_id<TTS_REMOVE_PARENS(Type)>())      \
-                            << " found "                                                           \
+      TTS_FAIL("Expecting " << ::tts::detail::white_(TTS_STRING(TTS_REMOVE_PARENS(T))) << " to be "\
+                            << ::tts::detail::white_(tts::type_id<TTS_REMOVE_PARENS(Type)>())      \
+                            << " but found "                                                       \
                             << ::tts::detail::red_(tts::type_id<TTS_REMOVE_PARENS(T)>())           \
           << " instead");                                                                          \
     }                                                                                              \
