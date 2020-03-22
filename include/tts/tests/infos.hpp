4ef424e6da87fbb3ee1758a963defa6b3a3a9889
Joel Falcou <joel.falcou@lri.fr>
2020-03-22 13:55:13 +0100

    Some more docs in markdeep format

--- include/tts/tests/infos.hpp
+++ include/tts/tests/infos.hpp
@@ -1,7 +1,7 @@
 //==================================================================================================
 /*
   TTS - Tiny Test System
-  Copyright 2019 Joel FALCOU
+  Copyright 2020 Joel FALCOU
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
@@ -12,51 +12,12 @@
 
 #include <tts/detail/infos.hpp>
 
-//==================================================================================================
-/**
-  * @brief Register a test which always passes and streams a custom Message string.
-  *
-  * **Required header:** `#include <tts/tests/infos.hpp>`
-  *
-  *  **Example:**
-  *
-  *  @snippet infos/pass.cpp pass
-  *
-  *  @param Message A literal string to display as additional informations
-**/
-//==================================================================================================
 #define TTS_PASS(Message)                                                                          \
   ::tts::detail::pass(runtime, {__FILE__, __LINE__}) << Message << std::endl /**/
 
-//==================================================================================================
-/**
-  * @brief Register a test which always fails and streams a custom message string.
-  *
-  * **Required header:** `#include <tts/tests/infos.hpp>`
-  *
-  *  **Example:**
-  *
-  *  @snippet infos/fail.cpp fail
-  *
-  *  @param Message A literal string to display as additional informations
-**/
-//==================================================================================================
 #define TTS_FAIL(Message)                                                                          \
   ::tts::detail::fail(runtime, {__FILE__, __LINE__}) << Message << std::endl /**/
 
-//==================================================================================================
-/**
-  * @brief Register a test as invalid and streams a custom message string.
-  *
-  * **Required header:** `#include <tts/tests/infos.hpp>`
-  *
-  *  **Example:**
-  *
-  *  @snippet infos/invalid.cpp invalid
-  *
-  *  @param Message A literal string to display as additional informations
-**/
-//==================================================================================================
 #define TTS_INVALID(Message)                                                                       \
   ::tts::detail::invalid(runtime, {__FILE__, __LINE__}) << Message << std::endl /**/
 
