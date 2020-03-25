117c185f56d928ec24647fc810b39866ef2623f2
Joel Falcou <joel.falcou@lri.fr>
2020-03-25 15:48:33 +0100

    :art: Remove out-dated macro

--- include/tts/tests/range.hpp
+++ include/tts/tests/range.hpp
@@ -345,14 +345,4 @@ namespace tts
   } while(::tts::detail::is_false())                                                                \
 /**/
 
-#define TTS_RANGE_CHECK(Producer, Ref, New)                                                         \
-  do                                                                                                \
-  {                                                                                                 \
-    if constexpr(std::is_floating_point_v<typename decltype(Producer)::base_type>)                  \
-      TTS_ULP_RANGE_CHECK(Producer, Ref, New, 2.0);                                                 \
-    else                                                                                            \
-      TTS_ULP_RANGE_CHECK(Producer, Ref, New, 0.0);                                                 \
-  } while(::tts::detail::is_false())                                                                \
-/**/
-
 #endif
