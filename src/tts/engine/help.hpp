1a0cf617400d5be3d88221b44f633cda13dab91c
Joel Falcou <joel.falcou@lri.fr>
2021-11-22 09:40:09 +0100

    Clean-up list of support CLI parameters

--- src/tts/engine/help.hpp
+++ src/tts/engine/help.hpp
@@ -20,14 +20,10 @@ namespace tts
     std::cout << "Usage: " << name  << " [OPTION...]\n";
     std::cout << "\nFlags:\n";
     std::cout << "  -h, --help        Display this help message\n";
-    std::cout << "  -n, --no-color    Disable colored output\n";
-    std::cout << "  -p, --pass        Report passing tests\n";
     std::cout << "  -x, --hex         Print the floating results in hexfloat mode\n";
     std::cout << "  -s, --scientific  Print the floating results in scientific mode\n";
     std::cout << "\nParameters:\n";
-    std::cout << "  --filter=str      Only run tests with `str` in their description\n";
     std::cout << "  --precision=arg   Set the precision for displaying floating pint values\n";
-    std::cout << "  --repeat=arg      Repeat each tests arg times\n";
     std::cout << "  --seed=arg        Set the PRNG seeds (default is time-based)\n";
     std::cout << "\nRange specifics Parameters:\n";
     std::cout << "  --block=arg       Set size of range checks samples (min. 32)\n";
