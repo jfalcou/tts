11a3c7bbad9511bc7c7f2c826a387f120742ca8d
Joel Falcou <joel.falcou@lri.fr>
2022-02-21 10:49:02 +0100

    Some compatibility fixes
    * Fix lambda issue in constexpr context
    * Make TTS compatible with libstdc++-10.3

--- include/tts/tts.hpp
+++ include/tts/tts.hpp
@@ -473,18 +473,22 @@ namespace tts
 }(EXPR)                                                                                             \
 
 #define TTS_CONSTEXPR_EXPECT(EXPR)                                                                  \
-[&]()                                                                                               \
+[]<typename C>(C )                                                                                  \
 {                                                                                                   \
-  static_assert((EXPR), "[TTS] - Error: " TTS_STRING(EXPR) " evaluates to false at compile-time."); \
+  static_assert ( C::value                                                                          \
+                , "[TTS] - Error: " TTS_STRING(EXPR) " evaluates to false at compile-time."         \
+                );                                                                                  \
   ::tts::global_runtime.pass();                                                                     \
-}()                                                                                                 \
+}(std::bool_constant<EXPR>{})                                                                       \
 
 #define TTS_CONSTEXPR_EXPECT_NOT(EXPR)                                                              \
-[&]()                                                                                               \
+[]<typename C>(C )                                                                                  \
 {                                                                                                   \
-  static_assert(!(EXPR), "[TTS] - Error: " TTS_STRING(EXPR) " evaluates to true at compile-time."); \
+  static_assert ( !C::value                                                                         \
+                , "[TTS] - Error: " TTS_STRING(EXPR) " evaluates to true at compile-time."          \
+                );                                                                                  \
   ::tts::global_runtime.pass();                                                                     \
-}()                                                                                                 \
+}(std::bool_constant<EXPR>{})                                                                       \
 
 namespace tts::detail
 {
@@ -702,10 +706,24 @@ namespace tts
 #include <cstdint>
 #include <type_traits>
 #include <utility>
+#if !defined(__cpp_lib_bit_cast)
+# include <cstring>
+#endif
 namespace tts::detail
 {
-  inline auto as_int(float a)   noexcept  { return std::bit_cast<std::int32_t>(a); }
-  inline auto as_int(double a)  noexcept  { return std::bit_cast<std::int64_t>(a); }
+#if !defined(__cpp_lib_bit_cast)
+  template <class To, class From>
+  To bit_cast(const From& src) noexcept requires(sizeof(To) == sizeof(From))
+  {
+    To dst;
+    std::memcpy(&dst, &src, sizeof(To));
+    return dst;
+  }
+#else
+  using std::bit_cast;
+#endif
+  inline auto as_int(float a)   noexcept  { return bit_cast<std::int32_t>(a); }
+  inline auto as_int(double a)  noexcept  { return bit_cast<std::int64_t>(a); }
   template<typename T> inline auto bitinteger(T a) noexcept
   {
     auto ia = as_int(a);
