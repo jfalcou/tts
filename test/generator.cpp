99f025d20cf5424d32c95e4ea9915758829b6ae3
Joel Falcou <joel.falcou@lri.fr>
2022-06-07 16:51:03 +0200

    Fix generators to handle output types faithfully

--- test/generator.cpp
+++ test/generator.cpp
@@ -20,7 +20,45 @@ template<typename T> struct array_of
   using types_list = typename make<T>::type;
 };
 
-TTS_CASE_WITH ( "Check behavior of add on wide"
+
+TTS_CASE_WITH ( "Check behavior for scalar types"
+              , ::tts::arithmetic_types
+              , ::tts::generate ( tts::value{37}
+                                , tts::ramp{65}, tts::ramp{1,2}
+                                , tts::reverse_ramp{10}, tts::reverse_ramp{100,2}
+                                , tts::between{0,100}
+                                , tts::randoms{0.,100.}
+                                , tts::sample{ std::uniform_real_distribution{0.,10.} }
+                                )
+              )
+<typename T>( T const& value
+            , T const& rmp  , T const& rmps
+            , T const& rrmp , T const& rrmps
+            , T const& btw
+            , auto const& rng, auto const& urng
+            )
+{
+  T value_ref = 37, rmp_ref = 65, rmps_ref = 1, rrmp_ref = 10, rrmps_ref = 100;
+  T btw_ref = 0;
+
+  TTS_EXPR_IS(value , T const&      );
+  TTS_EXPR_IS(rmp   , T const&      );
+  TTS_EXPR_IS(rmps  , T const&      );
+  TTS_EXPR_IS(rrmp  , T const&      );
+  TTS_EXPR_IS(rrmps , T const&      );
+  TTS_EXPR_IS(btw   , T const&      );
+  TTS_EXPR_IS(rng   , T const&      );
+  TTS_EXPR_IS(urng  , double const& );
+
+  TTS_EQUAL(value , value_ref );
+  TTS_EQUAL(rmp   , rmp_ref   );
+  TTS_EQUAL(rmps  , rmps_ref  );
+  TTS_EQUAL(rrmp  , rrmp_ref  );
+  TTS_EQUAL(rrmps , rrmps_ref );
+  TTS_EQUAL(btw   , btw_ref   );
+};
+
+TTS_CASE_WITH ( "Check behavior for non-scalar types"
               , array_of<::tts::arithmetic_types>
               , ::tts::generate ( tts::value{37}
                                 , tts::ramp{65}, tts::ramp{1,2}
@@ -34,7 +72,8 @@ TTS_CASE_WITH ( "Check behavior of add on wide"
             , T const& rmp  , T const& rmps
             , T const& rrmp , T const& rrmps
             , T const& btw
-            , T const& rng, T const& urng
+            , auto const& rng
+            , auto const& urng
             )
 {
   T value_ref { 37, 37, 37, 37, 37, 37, 37, 37, 37, 37}
@@ -53,6 +92,15 @@ TTS_CASE_WITH ( "Check behavior of add on wide"
   {
     btw_ref[i] = std::min( static_cast<v_t>(w1 + i*step), w2);
   }
+
+  TTS_EXPR_IS(value , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(rmp   , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(rmps  , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(rrmp  , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(rrmps , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(btw   , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(rng   , (std::array<v_t, 10> const&)    );
+  TTS_EXPR_IS(urng  , (std::array<double, 10> const&) );
 
   TTS_ALL_EQUAL(value , value_ref );
   TTS_ALL_EQUAL(rmp   , rmp_ref   );
