403ba81f81d467f9d239c646c1151271b4dec9c5
Joel Falcou <joel.falcou@lri.fr>
2022-06-29 10:52:58 +0200

    More changes to realistic PRNG

--- src/tts/tools/random.hpp
+++ src/tts/tools/random.hpp
@@ -17,32 +17,24 @@
 namespace tts
 {
   template<typename T>
-  struct realistic_distribution
+  struct fp_dist
   {
     using result_type = T;
     struct param_type
     {
       param_type( T pa = 0, T pb = 1
                 , std::size_t sz = 65536
-                , T mnp = 10
-                , T mxp = std::numeric_limits<T>::max()
+                , T mnp = std::numeric_limits<T>::epsilon()
+                , T mxp = 1./std::numeric_limits<T>::epsilon()
                 )
                 : a(pa), b(pb), n(sz), minpos(mnp), maxpos(mxp)
       {
         if(b<a) std::swap(a,b);
 
-        if constexpr(std::is_signed_v<T>)
-        {
-          if(a < -maxpos)             a = -maxpos;
-          else if(a>0 && a < minpos)  a =  minpos;
-          if(b > maxpos)              b =  maxpos;
-          else if(b<0 && b < -minpos) b = -minpos;
-        }
-        else
-        {
-          if(a>0 && a < minpos)  a =  minpos;
-          if(b > maxpos)         b =  maxpos;
-        }
+        if(a < -maxpos)             a = -maxpos;
+        else if(a>0 && a < minpos)  a =  minpos;
+        if(b > maxpos)              b =  maxpos;
+        else if(b<0 && b < -minpos) b = -minpos;
       }
 
       T a, b;
@@ -50,26 +42,12 @@ namespace tts
       T minpos, maxpos;
     };
 
-    realistic_distribution() noexcept : realistic_distribution(param_type{})
-    {}
-
-    realistic_distribution(T a, T b) noexcept : realistic_distribution(param_type{a,b})
-    {}
-
-    realistic_distribution(T a, T b, std::size_t n) noexcept : realistic_distribution(param_type{a,b,n})
-    {}
-
-    realistic_distribution(T a, T b, std::size_t n, T mn ) noexcept : realistic_distribution(param_type{a,b,n,mn})
-    {}
-
-    realistic_distribution(T a, T b, std::size_t n, T mn, T mx) noexcept : realistic_distribution(param_type{a,b,n,mn,mx})
-    {}
-
-
-    realistic_distribution(param_type const& pr) noexcept
-    {
-      param(pr);
-    }
+    fp_dist()                                     noexcept : fp_dist(param_type{}) {}
+    fp_dist(T a, T b)                             noexcept : fp_dist(param_type{a,b}) {}
+    fp_dist(T a, T b, std::size_t n)              noexcept : fp_dist(param_type{a,b,n}) {}
+    fp_dist(T a, T b, std::size_t n, T mn )       noexcept : fp_dist(param_type{a,b,n,mn}) {}
+    fp_dist(T a, T b, std::size_t n, T mn, T mx)  noexcept : fp_dist(param_type{a,b,n,mn,mx}) {}
+    fp_dist(param_type const& pr)                 noexcept { param(pr); }
 
     param_type const& param() const { return params; }
 
@@ -93,13 +71,6 @@ namespace tts
       return generate(limits[i],limits[i+1],p - *it,params.n,params.minpos);
     }
 
-    result_type operator()( int p )
-    {
-      auto it = std::upper_bound(sizes.begin(), sizes.end(), p) - 1;
-      auto i  = std::distance(sizes.begin(),it);
-      return generate(limits[i],limits[i+1],p - *it,params.n,params.minpos);
-    }
-
     tts::detail::block<T,7>                     limits;
     tts::detail::block<std::size_t,7>           sizes;
     std::uniform_int_distribution<std::size_t>  selector;
@@ -109,16 +80,7 @@ namespace tts
     void find_limits() noexcept
     {
       limits.clear();
-
-      auto zs = [&]()
-      {
-        if constexpr( std::floating_point<T>)
-          return std::array<T,7>{-params.maxpos,-1,-params.minpos,0,params.minpos,1,params.maxpos};
-        else if constexpr(std::is_signed_v<T>)
-          return std::array<T,5>{T(-params.maxpos),T(-params.minpos),T(0),params.minpos,params.maxpos};
-        else
-          return std::array<T,3>{ T(0),params.minpos,params.maxpos};
-      }();
+      std::array<T,7> zs{-params.maxpos,-1,-params.minpos,0,params.minpos,1,params.maxpos};
 
       for(auto z : zs)
       {
@@ -150,30 +112,30 @@ namespace tts
 
    static T generate(T va, T vb, std::size_t p, std::size_t n, T minpos) noexcept
     {
-      if constexpr( std::integral<T>)
+      const auto eval = [](double x, double y, double i, double sz) -> double
       {
-        const auto eval = [](double x, double y, double i, double sz) -> double
-        {
-          if(x>=1)  return std::exp2(std::lerp(std::log2(x), std::log2(y)   , i/sz));
-          else      return 0.;
-        };
+        if(x<1) return 1./std::exp2(std::lerp(std::log2(1./y), std::log2(1./x), i/sz));
+        else    return    std::exp2(std::lerp(std::log2(x)   , std::log2(y)   , i/sz));
+      };
 
-        if(va==0 || vb==0)  return static_cast<T>(p*minpos/(1.+minpos/2));
-        auto f = va<0 ? -1 : 1;
-        return std::round(f * eval(f * va, f * vb,p,n-1));
-      }
-      else
-      {
-        const auto eval = [](double x, double y, double i, double sz) -> double
-        {
-          if(x<1) return 1./std::exp2(std::lerp(std::log2(1./y), std::log2(1./x), i/sz));
-          else    return    std::exp2(std::lerp(std::log2(x)   , std::log2(y)   , i/sz));
-        };
-
-        if(va==0 || vb==0)  return 0.;
-        auto f = va<0 ? -1 : 1;
-        return f * eval(f * va, f * vb,p,n-1);
-      }
+      if(va==0 || vb==0)  return 0.;
+      auto f = va<0 ? -1 : 1;
+      return f * eval(f * va, f * vb,p,n-1);
     }
   };
+
+  template<typename T>
+  struct choose_distribution
+  {
+    using type = std::uniform_int_distribution<T>;
+  };
+
+  template<std::floating_point T>
+  struct choose_distribution<T>
+  {
+    using type = fp_dist<T>;
+  };
+
+  template<typename T>
+  using realistic_distribution = typename choose_distribution<T>::type;
 }
