1dd645e3c95d52b049dab87be748ef8896fd039a
Joel Falcou <joel.falcou@lri.fr>
2022-11-19 10:17:26 +0100

    Add typed tests

--- test/relation/not_equal.cpp
+++ test/relation/not_equal.cpp
@@ -22,6 +22,19 @@ TTS_CASE( "Check correctness of constexpr inequality tests" )
   TTS_CONSTEXPR_NOT_EQUAL(63,f());
 };
 
+TTS_CASE( "Check correctness of typed inequality tests" )
+{
+  TTS_TYPED_NOT_EQUAL(45.f,7.7f);
+
+  std::vector<float>  a{1,2,3}, b{1,2};
+  TTS_TYPED_NOT_EQUAL(a,b);
+};
+
+TTS_CASE( "Check correctness of constexpr typed inequality tests" )
+{
+  TTS_TYPED_CONSTEXPR_NOT_EQUAL(77,f());
+};
+
 #include "foo.hpp"
 
 TTS_CASE( "Check correctness of inequality tests on custom types" )
