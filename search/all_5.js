6a022e85d0af30d0d4cd354a406a86f7592db6ed
jfalcou <jfalcou@users.noreply.github.com>
2023-08-24 13:50:22 +0000

    deploy: 72f5befe43c15e5ce4c2d98830a3523aea7a8a6d

--- search/all_5.js
+++ search/all_5.js
@@ -4,5 +4,6 @@ var searchData=
   ['initialize_1',['initialize',['../namespacetts_a00fa573920d3dda291088d61899dab28.html#a00fa573920d3dda291088d61899dab28',1,'tts']]],
   ['int_5ftypes_2',['int_types',['../namespacetts.html#a213ac596b9a0b307be93bdc89f5ba738',1,'tts']]],
   ['integral_5ftypes_3',['integral_types',['../namespacetts.html#af94539e8b9b0bc71324e5a5155c97d40',1,'tts']]],
-  ['is_5fvalid_4',['is_valid',['../structtts_1_1options.html#a9548e8175531e8efa53c039e376ca902',1,'tts::options']]]
+  ['is_5fieee_5fequal_4',['is_ieee_equal',['../namespacetts_ab95e75c52ad7ef7f0281ab77b67f5ab5.html#ab95e75c52ad7ef7f0281ab77b67f5ab5',1,'tts']]],
+  ['is_5fvalid_5',['is_valid',['../structtts_1_1options.html#a9548e8175531e8efa53c039e376ca902',1,'tts::options']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['initialize_0',['initialize',['../namespacetts_a00fa573920d3dda291088d61899dab28.html#a00fa573920d3dda291088d61899dab28',1,'tts']]],
-  ['is_5fvalid_1',['is_valid',['../structtts_1_1options.html#a9548e8175531e8efa53c039e376ca902',1,'tts::options']]]
+  ['is_5fieee_5fequal_1',['is_ieee_equal',['../namespacetts_ab95e75c52ad7ef7f0281ab77b67f5ab5.html#ab95e75c52ad7ef7f0281ab77b67f5ab5',1,'tts']]],
+  ['is_5fvalid_2',['is_valid',['../structtts_1_1options.html#a9548e8175531e8efa53c039e376ca902',1,'tts::options']]]
 ];
