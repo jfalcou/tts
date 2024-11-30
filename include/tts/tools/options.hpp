6cb5563f9cde2cf88d31e95d01a454f32ef4fb1e
Joel Falcou <joel.falcou@lri.fr>
2024-11-30 22:24:23 +0100

    Back to main()

--- include/tts/tools/options.hpp
+++ include/tts/tools/options.hpp
@@ -7,50 +7,81 @@
 //======================================================================================================================
 #pragma once
 
-#include <chrono>
-#include <cmath>
-#include <initializer_list>
-#include <sstream>
-#include <string>
+#include <concepts>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-namespace tts
+namespace tts::_
 {
-  namespace detail
+  struct option
   {
-    struct option
+    option() = default;
+    option( const char* arg ) : token(arg), position(-1)
     {
-      option() = default;
-      option( std::string arg ) : token(std::move(arg)), position(token.rfind( '=' )) {}
+      auto it = strchr(arg,'=');
+      position = it ? (it - token) : strlen(token);
+    }
 
-      auto flag()     const { return token.substr(0, position); }
-      bool is_valid() const { return !flag().empty(); }
+    bool has_flag(const char* f) const
+    {
+      if(position == -1)      return false;
 
-      template<typename T> T get(T const& def = T{}) const
+      int len(strlen(f));
+      if(len > position)  return false;
+
+      return strncmp(token,f,position) == 0;
+    }
+
+    bool is_valid() const  { return position > 0;  }
+
+    template<typename T> T get(T const& def = T{}) const
+    {
+      T that = {};
+
+      if(is_valid())
       {
-        T that;
-
-        if(is_valid())
+        int n = 0;
+        if constexpr(std::integral<T>)
         {
-          std::istringstream os(token.substr(position+1));
-          if(os >> that) return that;
-          else           return def;
+          decltype(sizeof(void*)) v;
+          n = sscanf(token+position+1, "%ld", &v);
+          that = static_cast<T>(v);
+        }
+        else if constexpr(std::floating_point<T>)
+        {
+          double v;
+          n = sscanf(token+position+1, "%lf", &v);
+          that = static_cast<T>(v);
         }
         else
         {
-          return def;
+          n     = 1;
+          that  = T{token+position+1};
         }
+
+        if(n!=1) that = def;
+      }
+      else
+      {
+        that = def;
       }
 
-      std::string token     = "";
-      size_t      position  = std::string::npos;
-    };
-  }
+      return that;
+    }
 
+    const char* token     = "";
+    int         position  = -1;
+  };
+}
+
+namespace tts
+{
   //====================================================================================================================
   /*!
       @brief Command line arguments lightweight processor
 
-      @see arguments
+      @see option
 
       @groupheader{Example}
 
@@ -62,7 +93,7 @@ namespace tts
       {
         // Will fail if the test is not called via
         //  ./mytest.exe --pass--the-test or ./mytest.exe -W
-        TTS_EXPECT( (tts::arguments()[{"--pass--the-test","-W"}]) );
+        TTS_EXPECT( (tts::arguments()["--pass--the-test","-W"]) );
 
         // Will fail if the test is not called via
         //  ./mytest.exe --at_last
@@ -72,63 +103,71 @@ namespace tts
       TTS_CASE( "CLI test - setting" )
       {
         // Will fail if the test is not called via ./mytest.exe --xxx=13.37
-        TTS_EQUAL( tts::arguments().value("--xxx", 0.), 13.37);
+        TTS_EQUAL( tts::arguments().value<double>("--xxx"), 13.37);
 
         // Will fail if the test is not called via ./mytest.exe --www=9 or ./mytest.exe -zzz=9
-        TTS_EQUAL( (tts::arguments().value({"--www","-zzz"}, 0)), 9);
+        TTS_EQUAL( (tts::arguments().value(0,"--www","-zzz")), 9);
       };
       @endcode
   **/
   //====================================================================================================================
   struct options
   {
-    using params_t = std::initializer_list<const char*>;
-
-    /// Checks if any flag from with the strings `fs` is set on the command line
-    bool operator[](params_t    fs) const { return find(fs).is_valid(); }
-
     /// Checks if the flag `f` is set on the command line
-    bool operator[](const char* f ) const { return operator[]({f}); }
-
-    /// Returns a value of type `T` if a flag matches any of the strings in `fs` or `that` otherwise
-    template<typename T> T value(params_t fs, T that = {}) const
+    template<std::same_as<const char*>... Flags>
+    bool operator[](Flags... fs) const
     {
-      if( auto o = find(fs); o.is_valid()) that = o.template get<T>(that);
+      return find(fs...).is_valid();
+    }
+
+    /// Returns a value of type `T` if a flag matches any of the strings in `fs` or `T{}` otherwise
+    template<typename T, std::same_as<const char*>... Flags>
+    T value(Flags... fs) const
+    {
+      T that = {};
+      if( auto o = find(fs...); o.is_valid()) that = o.template get<T>(that);
       return that;
     }
 
-    /// Returns a value of type `T` if a flag matches the string `f` or `that` otherwise
-    template<typename T> T value(const char* f, T that = {}) const
+    /// Returns a value of type `T` if a flag matches any of the strings in `fs` or `that` otherwise
+    template<typename T, std::same_as<const char*>... Flags>
+    T value(T that, Flags... fs) const
     {
-      return value({f},that);
+      if( auto o = find(fs...); o.is_valid()) that = o.template get<T>(that);
+      return that;
     }
 
     /// Checks if current options set is not empty
     bool is_valid() { return argc && argv != nullptr; }
 
-    int argc;
-    char const** argv;
+    int           argc;
+    char const**  argv;
 
     private:
-
-    detail::option find(const char* f ) const { return find({f}); }
-    detail::option find(params_t    fs) const
+    template<std::same_as<const char*>... Flags> _::option find(Flags... fs) const
     {
+      const char* flags[] = {fs...};
+
       for(int i=1;i<argc;++i)
       {
-        detail::option o(argv[i]);
-        for(auto f : fs)
-          if( o.flag() == f ) return o;
+        _::option o(argv[i]);
+        for(auto f : flags)
+        {
+          if( o.has_flag(f) ) return o;
+        }
       }
 
-      return detail::option{};
+      return _::option{};
     }
   };
+}
 
-  namespace detail
+namespace tts
+{
+  namespace _
   {
-    inline ::tts::options current_arguments = {0,nullptr};
-    inline std::int32_t   current_seed      = -1;
+    inline options current_arguments = {0,nullptr};
+    inline int     current_seed      = -1;
   }
 
   //====================================================================================================================
@@ -143,9 +182,9 @@ namespace tts
   //====================================================================================================================
   inline void initialize(int argc, const char** argv)
   {
-    if(!detail::current_arguments.is_valid())
-      detail::current_arguments = ::tts::options{argc,argv};
+    if(!_::current_arguments.is_valid()) _::current_arguments = options{argc,argv};
   }
+
 
   //====================================================================================================================
   /*! @brief Retrieve the current list of command line argument
@@ -154,10 +193,7 @@ namespace tts
       @return The pre-processed command line arguments as a reference to a tts::options instance.
   **/
   //====================================================================================================================
-  inline ::tts::options const& arguments()
-  {
-    return detail::current_arguments;
-  }
+  inline options const& arguments() { return _::current_arguments; }
 
   //====================================================================================================================
   /*! @brief Initialize the random seed for tests
@@ -167,20 +203,15 @@ namespace tts
                         current elapsed time.
   **/
   //====================================================================================================================
-  inline std::int32_t random_seed(int base_seed = -1)
+  inline int random_seed(int base_seed = -1)
   {
-    if(detail::current_seed == -1)
+    if(_::current_seed == -1)
     {
-      auto s = ::tts::arguments().value( "--seed", base_seed );
-      if(s == -1 )
-      {
-        auto now = std::chrono::high_resolution_clock::now();
-        s        = static_cast<std::int32_t>(now.time_since_epoch().count());
-      }
-
-      detail::current_seed = s;
+      auto s = arguments().value( base_seed, "--seed" );
+      if(s == -1 ) s = static_cast<int>(time(0));
+      _::current_seed = s;
     }
 
-    return detail::current_seed;
+    return _::current_seed;
   }
 }
