ee1ffc06379498fe8bad3e98667bc87eb6520e49
Joel Falcou <joel.falcou@lri.fr>
2024-12-07 18:34:50 +0100

    Exceptions checks

--- standalone/tts/tts.hpp
+++ standalone/tts/tts.hpp
@@ -957,3 +957,52 @@ namespace tts::_
   }                                                                                                 \
 }()                                                                                                 \
 
+#define TTS_THROW_IMPL(EXPR, EXCEPTION, FAILURE)                                            \
+[&]()                                                                                       \
+{                                                                                           \
+  bool tts_caught = false;                                                                  \
+                                                                                            \
+  try                 { EXPR; }                                                             \
+  catch(EXCEPTION&  ) { tts_caught = true; }                                                \
+  catch(...)          { }                                                                   \
+                                                                                            \
+  if(tts_caught)                                                                            \
+  {                                                                                         \
+    TTS_PASS( "Expected: %s throws %s."                                                     \
+            , TTS_STRING(TTS_REMOVE_PARENS(EXPR)), TTS_STRING(TTS_REMOVE_PARENS(EXCEPTION)) \
+            );                                                                              \
+    ::tts::global_runtime.pass(); return ::tts::_::logger{false};                           \
+  }                                                                                         \
+  else                                                                                      \
+  {                                                                                         \
+    FAILURE ( "Expected: %s failed to throw %s."                                           \
+            , TTS_STRING(TTS_REMOVE_PARENS(EXPR)), TTS_STRING(TTS_REMOVE_PARENS(EXCEPTION)) \
+            );                                                                              \
+    return ::tts::_::logger{};                                                              \
+  }                                                                                         \
+}()
+#define TTS_THROW(EXPR, EXCEPTION, ...)     TTS_THROW_ ## __VA_ARGS__ ( EXPR, EXCEPTION )
+#define TTS_THROW_(EXPR, EXCEPTION)         TTS_THROW_IMPL(EXPR, EXCEPTION,TTS_FAIL)
+#define TTS_THROW_REQUIRED(EXPR, EXCEPTION) TTS_THROW_IMPL(EXPR, EXCEPTION,TTS_FATAL)
+#define TTS_NO_THROW_IMPL(EXPR,FAILURE)                                                     \
+[&]()                                                                                       \
+{                                                                                           \
+  bool tts_caught = false;                                                                  \
+                                                                                            \
+  try        { EXPR; }                                                                      \
+  catch(...) { tts_caught = true; }                                                         \
+                                                                                            \
+  if(!tts_caught)                                                                           \
+  {                                                                                         \
+    TTS_PASS( "Expected: %s does not throw.", TTS_STRING(TTS_REMOVE_PARENS(EXPR)) );        \
+    ::tts::global_runtime.pass(); return ::tts::_::logger{false};                           \
+  }                                                                                         \
+  else                                                                                      \
+  {                                                                                         \
+    FAILURE ( "Expected: %s throws unexpectedly.", TTS_STRING(TTS_REMOVE_PARENS(EXPR)));    \
+    return ::tts::_::logger{};                                                              \
+  }                                                                                         \
+}()
+#define TTS_NO_THROW(EXPR, ...)     TTS_NO_THROW_ ## __VA_ARGS__ ( EXPR )
+#define TTS_NO_THROW_(EXPR)         TTS_NO_THROW_IMPL(EXPR,TTS_FAIL)
+#define TTS_NO_THROW_REQUIRED(EXPR) TTS_NO_THROW_IMPL(EXPR,TTS_FATAL)
