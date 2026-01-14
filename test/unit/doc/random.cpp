fe897be15d87e74645fd74a61365e7115c7777bc
Joel Falcou <joel.falcou@lri.fr>
2026-01-14 16:06:47 +0100

    More random test

--- test/unit/doc/random.cpp
+++ test/unit/doc/random.cpp
@@ -13,11 +13,11 @@
 TTS_CASE_TPL("Check tts::random_value", float, double, int)
 <typename T>(tts::type<T>)
 {
-  for(auto i = 0; i < 10000; ++i)
+  for(auto i = 0; i < 100000; ++i)
   {
     T val = tts::random_value(T(-10), T(+10));
-    TTS_EXPECT(val >= T(-10));
-    TTS_EXPECT(val <= T(+10));
+    TTS_EXPECT(val >= T(-10)) << val;
+    TTS_EXPECT(val <= T(+10)) << val;
   }
 };
 //! [snippet]
